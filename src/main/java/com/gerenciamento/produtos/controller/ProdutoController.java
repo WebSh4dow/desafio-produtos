7a02a0b6df3aee6cd47dabcfe55a1e876314d85b
FireXtz <phantomrising@protonmail.com>
2023-12-24 03:01:51 -0300

    novas consultas adicionadas e tambem o cadastro de produtos

--- src/main/java/com/gerenciamento/produtos/controller/ProdutoController.java
+++ src/main/java/com/gerenciamento/produtos/controller/ProdutoController.java
@@ -1,12 +1,17 @@
 package com.gerenciamento.produtos.controller;
 
+import com.gerenciamento.produtos.exception.BussinesException;
 import com.gerenciamento.produtos.model.Produto;
 import com.gerenciamento.produtos.repository.ProdutoRepository;
+import com.gerenciamento.produtos.service.ProdutoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.List;
 
@@ -16,6 +21,11 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    Logger logger = LoggerFactory.getLogger(ProdutoController.class);
 
     @GetMapping("/filtrar/por-nome")
     public ResponseEntity<List<Produto>> consultarProdutoPorNome(@RequestParam("nome") String nome) {
@@ -38,5 +48,21 @@ public class ProdutoController {
 
         List<Produto> produtos = produtoRepository.filtrarProdutoPorNomeAndCategoriaAndDataCadastro(nome,categoria,dataCadastro);
         return new ResponseEntity<List<Produto>>(produtos, HttpStatus.OK);
+    }
+
+    @PostMapping("/cadastrar/novo-produto")
+    public ResponseEntity<?> cadastrar(@Valid @RequestBody Produto produto) {
+        try {
+            logger.info("Cadastrando novo produto: {}", produto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(produtoService.cadastrar(produto));
+
+        } catch (BussinesException e) {
+            logger.warn("Erro ao cadastrar produto: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+
+        } catch (Exception e) {
+            logger.error("Erro interno ao cadastrar produto", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocorreu um erro interno.");
+        }
     }
 }
--- src/main/java/com/gerenciamento/produtos/repository/query/ProdutoRepositoryQuery.java
+++ src/main/java/com/gerenciamento/produtos/repository/query/ProdutoRepositoryQuery.java
@@ -20,4 +20,26 @@ public interface ProdutoRepositoryQuery {
             "WHERE UPPER(TRIM(pr.nome)) LIKE %?1% " +
             "OR UPPER(TRIM(cat.nome)) LIKE %?2%" +
             "OR pr.dataCadastro = ?3";
+
+
+    public static String EXISTE_PRODUTO_COM_SKU_DUPLICADO =
+            "SELECT pr " +
+                    "FROM Produto " +
+                    "pr WHERE pr.sku = ?1";
+
+    public static String EXISTE_PRODUTO_COM_MESMO_NOME =
+            "SELECT pr " +
+                    "FROM Produto " +
+                    "pr WHERE pr.nome = ?1";
+
+    public static String CONSULTAR_PRODUTOS_ATIVOS =
+            "SELECT * FROM Produto " +
+                    "WHERE ativo = true";
+
+    public static String CONSULTAR_PRODUTOS_INATIVOS =
+            "SELECT * FROM Produto " +
+                    "WHERE ativo = false";
+
+
+
 }
